9b461ce3082d2d01a5bd17bbd0f8a0895c04f712
harshy261 <iyadavharsh261@gmail.com>
2025-01-01 04:42:26 +0530

    Annotation

--- 06_Annotation/Annotation.java
+++ 06_Annotation/Annotation.java
@@ -1,5 +1,28 @@
-package 06_Annotation;
+@Deprecated
+class A
+{
+	public void showTheDataWhichBelongsToThisClass()
+	{
+		System.out.println("in show A");
+	}
+}
+
+class B extends A 
+{
+	@Override
+//	public void showTheDataWhichBelongToThisClass()
+	public void showTheDataWhichBelongsToThisClass()
+
+	{
+		System.out.println("in show B");
+	}
+}
 
 public class Annotation {
-    
+    public static void main(String[] args) {
+    	
+    	B obj=new B();
+    	obj.showTheDataWhichBelongsToThisClass();
+    	
+    }
 }
