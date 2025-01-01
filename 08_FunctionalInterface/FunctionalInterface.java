ddd9ec028f1865b40f068f6dd65ab9f93f40a93f
harshy261 <iyadavharsh261@gmail.com>
2025-01-01 16:27:12 +0530

    Exception Handling

--- 08_FunctionalInterface/FunctionalInterface.java
+++ 08_FunctionalInterface/FunctionalInterface.java
@@ -1,5 +1,30 @@
-package 08_FunctionalInterface;
+@FunctionalInterface
+interface A
+{
+	void show();
+//	void run();
+}
+//class B implements A
+//{
+//	public void show()
+//	{
+//		System.out.println("in Show");
+//	}
+//}
+
 
 public class FunctionalInterface {
-    
+    public static void main(String[] args) {
+    	
+    	A obj=new A()
+    	{    	
+    		public void show()
+    		{
+    			System.out.println("in Show");
+    		}
+    	};
+ //   	A obj=new A();
+ //   	A obj=new B();
+    	obj.show();
+    }
 }
--- 10_Exception/Exception.java
+++ 10_Exception/Exception.java
@@ -1,5 +1,14 @@
-package 10_Exception;
+Types of error:-
+1. Complile - time error
+2. Runtime error -> Exception handling
+3. Logical error
+
 
 public class Exception {
+    public static void main(String[] args) {
     
+//  	System.out.Println();
+    	System.out.println(2+2);
+    	
+    }
 }
